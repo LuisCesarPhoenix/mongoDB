09bf071efef1774777dfe5e9b3f1cfef1d6feb93
LuisCesarPhoenix <cesarrodriguesgoncalves@hotmail.com>
2025-05-01 10:59:48 -0300

    :books: docs: Inclusão do arquivo escrever comando

--- escrever_comandos.js
+++ escrever_comandos.js
@@ -1,6 +1,6 @@
 db.carros.insertMany([
-    {},
-    {},
-    {},
-    {}
+    {_id: 4879, marca: "Volkswagen", modelo: "Gol GTi", ano_fabricacao: 1989, Kilometragem: "30 mil km"}, 
+    {_id: 8978, marca: "Honda", modelo: "Civic", ano_fabricacao: 2012, Kilometragem: "60 mil km"}, 
+    {_id: 7546, marca: "Toyota", modelo: "Corolla Sedan", ano_fabricacao: 2010, Kilometragem: "72 mil km"}, 
+    {_id: 2389, marca: "Chevrolet", modelo: "Onix Plus", ano_fabricacao: 2023 , Kilometragem: "45 mil km"}
 ])
